f075c8e2d3be3f4d8417e63844f587160b450e3b
¨Edgar <s.steinberg2019@gmail.com>
2024-08-16 12:14:58 -0300

    si

--- src/utils/multerUtil.js
+++ src/utils/multerUtil.js
@@ -1,24 +1,8 @@
 import multer from 'multer';
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         let folder = 'public/img'; // Default folder
-//         if (file.fieldname === 'docs') {
-//             folder = 'public/documents';
-//         } else if (file.fieldname === 'profile') {
-//             folder = 'public/profile';
-//         }
-//         cb(null, folder);
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.originalname);
-//     }
-// });
-
-// export const uploader = multer({ storage });
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        let folder = 'public/img'; // Carpeta por defecto
+        let folder = 'public/img'; // Default folder
         if (file.fieldname === 'docs') {
             folder = 'public/documents';
         } else if (file.fieldname === 'profile') {
@@ -27,7 +11,7 @@ const storage = multer.diskStorage({
         cb(null, folder);
     },
     filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, file.originalname);
     }
 });
 

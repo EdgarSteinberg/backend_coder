b680c86c0521666aa42616f36d6471d34c5a10d0
¨Edgar <s.steinberg2019@gmail.com>
2024-08-17 14:44:00 -0300

    porfa

--- src/utils/multerUtil.js
+++ src/utils/multerUtil.js
@@ -1,14 +1,40 @@
+// import multer from 'multer';
+// import __dirname from './constantsUtil';
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         let folder = 'public/img'; // Default folder
+//         if (file.fieldname === 'docs') {
+//             folder = 'public/documents';
+//         } else if (file.fieldname === 'profile') {
+//             folder = 'public/profile';
+//         }
+//         //cb(null, folder);
+//         cb(null,(`${__dirname} '../../'`, folder));
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, file.originalname);
+//     }
+// });
+
+// export const uploader = multer({ storage });
+
 import multer from 'multer';
+import path from 'path';
+import __dirname from './constantsUtil.js';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        let folder = 'public/img'; // Default folder
+        let folder = 'public/img'; // Carpeta predeterminada
         if (file.fieldname === 'docs') {
             folder = 'public/documents';
         } else if (file.fieldname === 'profile') {
             folder = 'public/profile';
         }
-        cb(null, folder);
+        // Construir la ruta completa usando path.join y __dirname
+        const fullPath = path.join(__dirname, '../../', folder);
+        console.log(`Ruta de carga de archivo: ${fullPath}`); // Mensaje de depuración (opcional)
+        cb(null, fullPath);
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
